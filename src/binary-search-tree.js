a7d955637bc7040f27f1227574b5b05155bbcadd
Alyona8891 <besssta888@mail.ru>
2023-04-17 03:25:46 +0300

    feat: add my solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError, testOptional } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -14,7 +14,7 @@ class BinarySearchTree {
   root() {
     //throw new NotImplementedError('Not implemented');
     if (this.rootTree !== null) {
-      return this.rootTree();
+      return this.rootTree;
     } else {
       return null;
     }
@@ -22,22 +22,80 @@ class BinarySearchTree {
 
   add(data) {
     //throw new NotImplementedError('Not implemented');
-    
+    this.rootTree = addEl(this.rootTree, data);
+    function addEl(node, value) {
+      if(!node) {
+        return new Node(value);
+      }
+      if(node.value === value) {
+        return node;
+      }
+      if(value < node.value) {
+        node.left = addEl(node.left, value);
+      } else {
+        node.right = addEl(node.right, value);
+      }
+      return node;
+    }
   }
 
   has(data) {
     //throw new NotImplementedError('Not implemented');
-    
+    return searchEl(this.rootTree, data);
+    function searchEl(node, value) {
+    if(!node) {
+      return false;
+    } 
+    if(node.value === value) {
+      return true;
+    }
+    if(value < node.value) {
+      return searchEl(node.left, value);
+    } else {
+      return searchEl(node.right, value);
+    }
+  }
   }
 
   find(data) {
-    //throw new NotImplementedError('Not implemented');
+    
     
   }
 
   remove(data) {
     //throw new NotImplementedError('Not implemented');
-    
+    this.rootTree = removeEl(this.rootTree, data);
+    function removeEl(node, value) {
+      if(!node) {
+        return null;
+      }
+      if(value < node.value) {
+        node.left = removeEl(node.left, value);
+        return node;
+      } else if (value > node.value) {
+        node.right = removeEl(node.right, value);
+        return node;
+      } else {
+        if(!node.left && !node.right) {
+          return null;
+        }
+        if(!node.left) {
+          node = node.right;
+          return node;
+        }
+        if(!node.right) {
+          node = node.left;
+          return node;
+        }
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+        node.value = minFromRight.value;
+        node.right = removeEl(node.right, minFromRight.value);
+        return node;
+      }
+    }
   }
 
   min() {
