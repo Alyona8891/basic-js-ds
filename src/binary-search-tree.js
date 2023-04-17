8db30708d39b42946a5004154e982a29e7d11465
Alyona8891 <besssta888@mail.ru>
2023-04-17 18:10:53 +0300

    feat: add my solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,95 +7,36 @@ const { Node } = require('../extensions/list-tree.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-  constructor() {
-    this.rootTree = null;
+   constructor() {
+    this.rootMy = null;
   }
 
   root() {
-    //throw new NotImplementedError('Not implemented');
-    if (this.rootTree !== null) {
-      return this.rootTree;
-    } else {
+    if (!this.rootMy) {
       return null;
+    } else {
+      return this.rootMy;
     }
   }
 
   add(data) {
     //throw new NotImplementedError('Not implemented');
-    this.rootTree = addEl(this.rootTree, data);
-    function addEl(node, value) {
-      if(!node) {
-        return new Node(value);
-      }
-      if(node.value === value) {
-        return node;
-      }
-      if(value < node.value) {
-        node.left = addEl(node.left, value);
-      } else {
-        node.right = addEl(node.right, value);
-      }
-      return node;
-    }
+    ;
   }
 
-  has(data) {
+  has(value) {
     //throw new NotImplementedError('Not implemented');
-    return searchEl(this.rootTree, data);
-    function searchEl(node, value) {
-    if(!node) {
-      return false;
-    } 
-    if(node.value === value) {
-      return true;
-    }
-    if(value < node.value) {
-      return searchEl(node.left, value);
-    } else {
-      return searchEl(node.right, value);
-    }
-  }
+   
   }
 
-  find(data) {
-    
-    
-  }
-
-  remove(data) {
+  find(value) {
     //throw new NotImplementedError('Not implemented');
-    this.rootTree = removeEl(this.rootTree, data);
-    function removeEl(node, value) {
-      if(!node) {
-        return null;
-      }
-      if(value < node.value) {
-        node.left = removeEl(node.left, value);
-        return node;
-      } else if (value > node.value) {
-        node.right = removeEl(node.right, value);
-        return node;
-      } else {
-        if(!node.left && !node.right) {
-          return null;
-        }
-        if(!node.left) {
-          node = node.right;
-          return node;
-        }
-        if(!node.right) {
-          node = node.left;
-          return node;
-        }
-        let minFromRight = node.right;
-        while (minFromRight.left) {
-          minFromRight = minFromRight.left;
-        }
-        node.value = minFromRight.value;
-        node.right = removeEl(node.right, minFromRight.value);
-        return node;
-      }
-    }
+   
+}
+
+  remove(value) {
+    //throw new NotImplementedError('Not implemented');
+    
   }
 
   min() {
@@ -107,6 +48,7 @@ class BinarySearchTree {
     //throw new NotImplementedError('Not implemented');
     
   }
+  
 }
 
 module.exports = {
